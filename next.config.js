453ee6d7a88ebed26ec66e2ec4b461179a77796d
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2018-08-25 13:34:23 -0700

    3.2.0

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const setIn = require('lodash/set');
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Perform customizations to webpack config
+
+    setIn(config, 'node.fs', 'empty');
+    // Important: return the modified config
+    return config;
+  },
+  webpackDevMiddleware: config => {
+    // Perform customizations to webpack dev middleware config
+
+    // Important: return the modified config
+    return config;
+  },
+};
