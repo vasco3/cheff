f70f5d5f042cba6d70ec417c0e41b4a7f7045482
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2018-08-26 18:04:13 -0700

    Implement router (major)

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+import Layout from '../src/Layout';
+
+const PAGE_TITLES = {
+  default: 'Meal Plan',
+  about: 'About',
+  recipes: 'Recipes',
+};
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+    return (
+      <Container>
+        <Layout
+          pageTitle={
+            PAGE_TITLES[router.pathname.replace(/\//g, '')] ||
+            PAGE_TITLES.default
+          }
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
