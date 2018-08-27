e914bded862380c00b7b1dca08cb2ffcae981368
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2018-08-26 18:43:12 -0700

    Add about page (major)

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import { Typography } from 'rmwc';
+
+function AboutPage() {
+  return (
+    <div className="m-4">
+      <Typography use="body1" tag="div" className="mt-4">
+        Meal Planner for calories and macros
+      </Typography>
+
+      <Typography use="body2" tag="div" className="mt-4">
+        Inspired in KinoCheff from{' '}
+        <a href="https://www.kinobody.com" target="_blank">
+          kinobody
+        </a>
+        .
+      </Typography>
+    </div>
+  );
+}
+export default AboutPage;
