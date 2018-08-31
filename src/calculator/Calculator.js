45d808390ee829a6e48b6f95183d2eab0ae94f53
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2018-08-30 23:31:08 -0700

    Update styles in Calculator (minor)

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from 'next/link';
 import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 import { Button } from 'rmwc/Button';
-// import { ListDivider } from 'rmwc/List';
+import { ListDivider } from 'rmwc/List';
 import { Select } from 'rmwc';
 import { TextField, TextFieldHelperText } from 'rmwc/TextField';
 import { Typography } from 'rmwc/Typography';
@@ -60,68 +61,83 @@ class Calculator extends React.Component {
               <Typography use="overline" tag="div" className="px-4">
                 Kinobody Calories / Macros (Daily Targets)
               </Typography>
+              <ListDivider />
               <Form>
-                <Select
-                  label="Kinobody Program"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  options={[{ label: 'GGP - Greek God Program', value: 'GGP' }]}
-                  outlined
-                  value={values.kinobodyProgram}
-                />
-
-                <Select
-                  label="Program Mode"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  options={kinobodyProgramModeOptions}
-                  outlined
-                  name="kinobodyProgramMode"
-                  value={values.kinobodyProgramMode}
-                />
-
-                <Select
-                  label="Macro Options"
-                  name="kinobodyMacroOption"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  options={macroOptions}
-                  outlined
-                  value={values.kinobodyMacroOption}
-                />
-
-                <TextField
-                  label="Body weight"
-                  name="bodyWeight"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  outlined
-                  value={values.bodyWeight}
-                />
-                <TextFieldHelperText validationMsg persistent>
-                  {errors.bodyWeight}
-                </TextFieldHelperText>
-
-                <Select
-                  label="units"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  options={['lbs', 'kg']}
-                  outlined
-                  name="bodyWeightIsInLbs"
-                  value={values.bodyWeightIsInLbs}
-                />
-
-                <footer className="flex justify-end items-center mr-4">
-                  <TextFieldHelperText persistent>
-                    {this.state.isSaved && 'Saved'}
-                  </TextFieldHelperText>
-                  <Button onClick={handleReset}>reset</Button>
-                  <Button type="submit" disabled={isSubmitting}>
-                    save
-                  </Button>
-                </footer>
+                <div className="form">
+                  <Select
+                    label="Kinobody Program"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    options={[
+                      { label: 'GGP - Greek God Program', value: 'GGP' },
+                    ]}
+                    outlined
+                    value={values.kinobodyProgram}
+                  />
+                  <Select
+                    label="Program Mode"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    options={kinobodyProgramModeOptions}
+                    outlined
+                    name="kinobodyProgramMode"
+                    value={values.kinobodyProgramMode}
+                  />
+                  <Select
+                    label="Macro Options"
+                    name="kinobodyMacroOption"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    options={macroOptions}
+                    outlined
+                    value={values.kinobodyMacroOption}
+                  />
+                  <TextField
+                    label="Body weight"
+                    name="bodyWeight"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    outlined
+                    value={values.bodyWeight}
+                  />
+                  {errors.bodyWeight && (
+                    <TextFieldHelperText validationMsg persistent>
+                      {errors.bodyWeight}
+                    </TextFieldHelperText>
+                  )}
+                  <Select
+                    label="units"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    options={['lbs', 'kg']}
+                    outlined
+                    name="bodyWeightIsInLbs"
+                    value={values.bodyWeightIsInLbs}
+                  />
+                  <footer className="flex justify-end">
+                    <Button onClick={handleReset}>reset</Button>
+                    <Button type="submit" disabled={isSubmitting}>
+                      save
+                    </Button>
+                  </footer>
+                  {this.state.isSaved && (
+                    <Typography use="headline6" tag="div">
+                      Saved! Now continue to{' '}
+                      <Link href="/recipes">Recipes</Link> or{' '}
+                      <Link href="/plan">Meal Plan</Link>
+                    </Typography>
+                  )}
+                </div>
               </Form>
+              <style jsx>{`
+                .form {
+                  display: grid;
+                  margin: 1rem;
+                  grid-template-columns: 1fr;
+                  grid-gap: 1rem;
+                  max-width: 400px;
+                }
+              `}</style>
             </div>
           );
         }}
